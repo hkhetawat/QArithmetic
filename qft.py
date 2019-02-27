66ae062a414737b2853ace05c4290fa5515fd4f4
Tom Wong <proofrific@gmail.com>
2019-02-27 16:19:58 -0600

    Reprogrammed the quantum Fourier transform, and added the inverse.

--- qft.py
+++ qft.py
@@ -1,7 +1,26 @@
-# From https://github.com/Qiskit/qiskit-terra/blob/master/examples/python/qft.py
+from math import pi
+
+# See https://en.wikipedia.org/wiki/Quantum_Fourier_transform#Circuit_implementation
+# for a circuit diagram.
+
 def qft(circ, q, n):
-    """n-qubit QFT on q in circ."""
-    for j in range(n):
-        for k in range(j):
-            circ.cu1(math.pi/float(2**(j-k)), q[j], q[k])
-        circ.h(q[j])
+    # Iterate through the target.
+    for i in range(1,n+1):
+        # Apply the Hadamard gate to the target.
+        circ.h(q[i-1])
+
+        # Iterate through the control.
+        for j in range(i+1,n+1):
+            circ.cu1(2*pi/2**j, q[j-1], q[i-1])
+
+# The inverse Fourier transform just uses a negative phase.
+def iqft(circ, q, n):
+    # Iterate through the target.
+    for i in range(n,0,-1):
+        # Iterate through the control.
+        for j in range(n,i,-1):
+            circ.cu1(-2*pi/2**j, q[j-1], q[i-1])
+
+        # Apply the Hadamard gate to the target.
+        circ.h(q[i-1])
+

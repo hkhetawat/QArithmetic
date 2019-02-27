32bcbc22d32c9ee204024881e3c3733f004d0537
Tom Wong <proofrific@gmail.com>
2019-02-27 15:01:22 -0600

    Added a function for the quantum fourier transform.

---
+++ qft.py
@@ -0,0 +1,7 @@
+# From https://github.com/Qiskit/qiskit-terra/blob/master/examples/python/qft.py
+def qft(circ, q, n):
+    """n-qubit QFT on q in circ."""
+    for j in range(n):
+        for k in range(j):
+            circ.cu1(math.pi/float(2**(j-k)), q[j], q[k])
+        circ.h(q[j])

38f3c293407fa7a2148417b7a54c75a25e065562
Tom Wong <proofrific@gmail.com>
2019-02-28 09:17:49 -0600

    Moved the multiplication functions to QArithmetic.py.

--- examples/test_mult.py
+++ examples/test_mult.py
@@ -1,53 +1,7 @@
 # Import the Qiskit SDK
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import execute, Aer
-
-# Controlled operations
-
-def cccx(ctrl,a,b,c):
-    qc.ccx(ctrl,a,anc[0])
-    qc.ccx(b,anc[0],c)
-    qc.ccx(ctrl,a,anc[0])
-    qc.ccx(b,anc[0],c)
-
-def csum(circ, ctrl, cin, a, b):
-    circ.ccx(ctrl,a,b)
-    circ.ccx(ctrl,cin,b)
-
-def ccarry(circ, ctrl, cin, a, b, cout):
-    cccx(ctrl, a, b, cout)
-    circ.ccx(ctrl, a, b)
-    cccx(ctrl, cin, b, cout)
-
-def ccarry_dg(circ, ctrl, cin, a, b, cout):
-    cccx(ctrl, cin, b, cout)
-    circ.ccx(ctrl, a, b)
-    cccx(ctrl, a, b, cout)
-
-def c_adder(circ, ctrl, a, b, n):
-    for i in range(0, n-1):
-        ccarry(circ, ctrl, c[i], a[i], b[i], c[i+1])
-    ccarry(circ, ctrl, c[n-1], a[n-1], b[n-1], b[n])
-    
-    circ.ccx(ctrl, c[n-1],b[n-1])
-    
-    for i in range(n-2,-1,-1):
-        ccarry_dg(circ, ctrl, c[i], a[i], b[i], c[i+1])
-        csum(circ, ctrl, c[i], a[i], b[i])
-
-def sub_qr(qr, x, y):
-    sub = []
-    for i in range (x, y+1):
-        sub = sub + [(qr, i)]
-    return sub        
-
-
-def c_multiplier(circ, a, b, m, n):
-    for i in range (0, n):
-        c_adder(circ, a[i], b, sub_qr(m, i, n+i), n)
-
-
-
+from QArithmetic import c_multiplier
 
 # Input N
 N = 3

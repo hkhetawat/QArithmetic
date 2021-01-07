e51046ec259cb529c5ded416dc7e0f85ac168f6a
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2021-01-06 20:07:04 -0500

    Updated Tests To Reflect Changes

--- examples/test_rev_pow.py
+++ examples/test_rev_pow.py
@@ -1,6 +1,6 @@
 # Import the Qiskit SDK
 from numpy.lib.function_base import copy
-from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, IBMQ
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import execute, Aer
 from qiskit.providers.aer import QasmSimulator
 from QArithmetic import full_qr, power
@@ -20,7 +20,6 @@ ca = ClassicalRegister(N)
 cm = ClassicalRegister(N*(pow(2,X)-1))
 
 qc = QuantumCircuit(a, b, m, cm, ca)
-# b = full_qr(b) + full_qr(b)
 
 # Input
 # a = 11 = 3
@@ -32,22 +31,13 @@ qc.x(b[1])
 
 power(qc, a, b, m)
 
-# qc = qc.decompose()
-# qc = qc.reverse_ops()
-
 qc.measure(m, cm)
 qc.measure(a, ca)
 
-circuit_drawer(qc, output="text", interactive=True, scale=1.0, filename='test3', fold=-1)
-# Only 2 qubuts ^ 2 qubits would use 2^48Bytes~35TB in a statevector
-# Thus MPS must be used here and if not configured correctly it may act unexpectedly
-provider = IBMQ.load_account()
-backend_sim = provider.backends.ibmq_qasm_simulator
-# backend_sim = QasmSimulator(method='extended_stabilizer',precision="single", max_memory_mb=int(pow(2,33.5)))
+backend_sim = Aer.get_backend('qasm_simulator')
 print("started job")
 
 job_sim = execute(qc, backend_sim, shots=1)
 result_sim = job_sim.result()
 
 print(result_sim.get_counts(qc))
-print()
